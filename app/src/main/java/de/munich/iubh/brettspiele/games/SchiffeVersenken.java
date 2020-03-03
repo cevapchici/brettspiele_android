7c539234ae45d7f2d15cc0cbceed9cf738f40f81
chi <maih>
2020-03-03 21:31:08 +0100

    Removed toolbar from activity

--- app/src/main/java/de/munich/iubh/brettspiele/games/SchiffeVersenken.java
+++ app/src/main/java/de/munich/iubh/brettspiele/games/SchiffeVersenken.java
@@ -15,8 +15,6 @@ public class SchiffeVersenken extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schiffe_versenken);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
